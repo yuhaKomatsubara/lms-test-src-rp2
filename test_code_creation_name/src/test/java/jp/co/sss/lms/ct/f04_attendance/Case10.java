45f5dfd4dfed3c1d6b00dc5f9eab85325c6d4bee
Yuha Komatsubara <y.komatsubara.its.202507@gmail.com>
2025-10-27 13:09:52 +0900

    fix:Case10.java 勤怠管理のテストコード追記 WebElementを利用して勤怠入力が可能かをテスト

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +40,123 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログイン要素の取得
+		WebElement id = webDriver.findElement(By.name("loginId"));
+		WebElement password = webDriver.findElement(By.name("password"));
+
+		//クリア処理
+		id.clear();
+		password.clear();
+
+		//ログイン情報の入力
+		id.sendKeys("StudentAA01");
+		password.sendKeys("StudentAA001");
+
+		//ログインボタンを押下
+		WebElement button = webDriver.findElement(By.className("btn-primary"));
+		button.click();
+
+		//URLチェック
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//勤怠管理画面に遷移
+		WebElement attendanc = webDriver.findElement(By.linkText("勤怠"));
+		attendanc.click();
+
+		// 5秒待つ
+		//		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		//		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		//		alert.accept();
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		//URLチェック・エビデンス
+		assertEquals("http://localhost:8080/lms/attendance/detail", webDriver.getCurrentUrl());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		//出勤ボタンを押下
+		WebElement push = webDriver.findElement(By.name("punchIn"));
+		push.click();
+
+		// 5秒待つ
+		pageLoadTimeout(5);
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		WebElement button = webDriver.findElement(By.className("alert-dismissible"));
+		assertEquals("×\n勤怠情報の登録が完了しました。", button.getText());
+
+		// 出勤完了メッセージのエビデンス取得
+		getEvidence(new Object() {
+		}, "01");
+
+		pageLoadTimeout(10);
+
+		WebElement tabele = webDriver.findElement(By.tagName("tbody"));
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].scrollHeight;", tabele);
+
+		scrollBy("window.innerHeight");
+		pageLoadTimeout(8);
+		getEvidence(new Object() {
+		}, "02");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		scrollTo("0");
+		//退勤ボタンを押下
+		WebElement push = webDriver.findElement(By.name("punchOut"));
+		push.click();
+
+		// 5秒待つ
+		pageLoadTimeout(5);
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		WebElement comp = webDriver.findElement(By.className("alert-dismissible"));
+		assertEquals("×\n勤怠情報の登録が完了しました。", comp.getText());
+
+		// 退勤完了のエビデンス取得
+		getEvidence(new Object() {
+		}, "01");
+
+		WebElement table = webDriver.findElement(By.tagName("tbody"));
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].scrollHeight;", table);
+
+		scrollBy("window.innerHeight");
+		pageLoadTimeout(8);
+		getEvidence(new Object() {
+		}, "02");
 	}
 
 }
