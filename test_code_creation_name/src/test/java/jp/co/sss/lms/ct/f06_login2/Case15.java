86a7ecb7fb127bb4c2b25634edc6a15847c4ca82
Yuha Komatsubara <y.komatsubara.its.202507@gmail.com>
2025-10-28 16:12:50 +0900

    fix:Case15.java 利用規約同意チェックのテストコード追記 WebElementを利用し、利用規約の同意チェックを実施

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +40,59 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//画面遷移
+		goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログイン要素の取得
+		WebElement id = webDriver.findElement(By.name("loginId"));
+		WebElement password = webDriver.findElement(By.name("password"));
+
+		//クリア処理
+		id.clear();
+		password.clear();
+
+		//ログイン情報の入力
+		id.sendKeys("StudentAA04");
+		password.sendKeys("StudentAA04");
+
+		//ログインボタンを押下
+		WebElement buttonElement = webDriver.findElement(By.className("btn-primary"));
+		buttonElement.click();
+
+		//URLチェック
+		assertThat(webDriver.getTitle(), is(containsString("セキュリティ規約")));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+		scrollBy("100");
+		//次へを押下
+		WebElement next = webDriver.findElement(By.className("btn-primary"));
+		next.click();
+
+		getEvidence(new Object() {
+		}, "01");
+
+		scrollBy("150");
+
+		//エビデンス取得・エラーチェック
+		getEvidence(new Object() {
+		}, "02");
+		WebElement agreeError = webDriver.findElement(By.className("error"));
+		assertThat(agreeError.getText(), is(containsString("セキュリティ規約への同意は必須です。")));
+
 	}
 
 }
